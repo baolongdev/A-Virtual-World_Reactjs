dc8213ae0e0f867c2d0b13630bc0618c1af1c4e3
Lê Bảo Long <longle12042006a@gmail.com>
2024-02-17 21:25:39 +0700

    polygon done

--- scripts/world/primitives/polygon.ts
+++ scripts/world/primitives/polygon.ts
@@ -1,11 +1,11 @@
-import { getIntersection, average, getRandomColor } from "../math/utils";
+import { getIntersection, average, getRandomColor, dot, normalize, subtract } from "../math/utils";
 import { Point } from "./point";
 import { Segment } from "./segment";
-
 
 export class Polygon {
     points: Point[]
     segments: Segment[]
+    id: number
 
     constructor(points: Point[]) {
         this.points = points
@@ -43,6 +43,7 @@ export class Polygon {
                 }
             }
         }
+        let end = performance.now();
         return keptSegments;
     }
 
@@ -59,7 +60,10 @@ export class Polygon {
         for (let i = 0; i < segs1.length; i++) {
             for (let j = 0; j < segs2.length; j++) {
                 const int = getIntersection(
-                    segs1[i].p1, segs1[i].p2, segs2[j].p1, segs2[j].p2
+                    segs1[i].p1,
+                    segs1[i].p2,
+                    segs2[j].p1,
+                    segs2[j].p2
                 );
 
                 if (int && int.offset != 1 && int.offset != 0) {
@@ -75,12 +79,72 @@ export class Polygon {
         }
     }
 
+    simplify() {
+        for (let i = 1; i < this.points.length; i++) {
+            const prev = this.points[i - 1];
+            const cur = this.points[i];
+            const next = this.points[(i + 1) % this.points.length];
+            if (dot(normalize(subtract(cur, prev)), normalize(subtract(next, cur))) >= 0.99) {
+                this.points.splice(i, 1);
+                i--;
+            }
+        }
+        this.segments = [];
+        for (let i = 1; i <= this.points.length; i++) {
+            this.segments.push(
+                new Segment(this.points[i - 1], this.points[i % this.points.length])
+            );
+        }
+    }
+
+    area() {
+        let area = 0;
+        for (let i = 0; i < this.points.length; i++) {
+            let nextI = i + 1;
+            if (nextI == this.points.length) {
+                nextI = 0;
+            }
+            area += this.points[i].x * this.points[nextI].y;
+            area -= this.points[i].y * this.points[nextI].x;
+        }
+        area = Math.abs(area) / 2
+        return area;
+    }
+
     distanceToPoint(point: Point) {
         return Math.min(...this.segments.map((s) => s.distanceToPoint(point)))
     }
 
     distanceToPoly(poly: Polygon) {
         return Math.min(...this.points.map((p) => poly.distanceToPoint(p)))
+    }
+
+    intersectsSegment(seg) {
+        for (let s1 of this.segments) {
+            if (getIntersection(s1.p1, s1.p2, seg.p1, seg.p2)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // containsSegment(seg: Segment) {
+    //     if (this.containsPoint(seg.p1)) {
+    //         return true;
+    //     }
+    //     if (this.containsPoint(seg.p2)) {
+    //         return true;
+    //     }
+    //     return false;
+    // }
+
+    containsPoly(poly: Polygon) {
+        for (const p of poly.points) {
+            if (this.containsPoint(p)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     intersectsPoly(poly: Polygon) {
